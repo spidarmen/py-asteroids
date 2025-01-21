9636f74d5efe48f9fab1bc568f1bec0c56846601
spidarmen <git@verycool.fun>
2025-01-21 12:37:20 -0800

    implemented asteroid objects

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-    pygame.display.set_caption("Asteroids")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
     game_clock = pygame.time.Clock()
-
-    BLACK = (0, 0, 0)
-    dt    = 0
 
     #create groups
     updatable = pygame.sprite.Group()
     drawable  = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    BLACK = (0, 0, 0)
+    dt    = 0
 
     while True:
         for event in pygame.event.get():
@@ -29,12 +34,12 @@ def main():
             
         for i in updatable:
             i.update(dt)
+        
+        screen.fill(BLACK)
 
         for i in drawable:
             i.draw(screen)
 
-        screen.fill(BLACK)
-        player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
