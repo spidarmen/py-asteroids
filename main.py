9486582b49118c63dd3cad7120c680470fa769b9
spidarmen <git@verycool.fun>
2025-01-20 23:02:03 -0800

    added frame limit

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ def main():
     pygame.init()
     pygame.display.set_caption("Asteroids")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+
     BLACK = (0, 0, 0)
+    dt    = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +20,7 @@ def main():
                 return
         screen.fill(BLACK)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
